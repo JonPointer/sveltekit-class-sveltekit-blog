24a776334e13affba635eecdde8e47068113c6d6
Jon Pointer <jonpointer@msn.com>
2023-03-21 15:44:32 -0600

    After video 15, sequencing hooks

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,10 +1,36 @@
-import {auth} from '$db/fake_auth';
+import { sequence } from '@sveltejs/kit/hooks';
+import { auth } from '$db/fake_auth';
 
-export async function handle({event, resolve}) {
-    const user=auth();
-    event.locals.user = user;
-    event.locals.this_is_just_a_test = 'test';
-    // console.log('event', event);
-    event.cookies.set('test', 'test');
-    return resolve(event);
+// runs 1st
+async function logger({ event, resolve }) {
+	const start_time = Date.now();
+	// Wait on response, run other hooks and load
+	const response = await resolve(event);
+
+	console.log(`${Date.now() - start_time}ms  ${event.request.method} ${event.url.pathname}`);
+
+	return response;
 }
+
+// runs 2nd
+function authorize({ event, resolve }) {
+	const user = auth();
+	event.locals.user = user;
+	return resolve(event);
+}
+
+export const handle = sequence(logger, authorize);
+
+// Intercepting Fetch
+// export function handleFetch({ request, fetch }) {
+// 	return fetch(request);
+// }
+
+// Intercept Errors
+// export function handleError({ error, event }) {
+// 	// logger(error, event)
+// 	return {
+// 		message: 'Oops, im intercepting in a hook',
+// 		code: error.code
+// 	};
+// }
