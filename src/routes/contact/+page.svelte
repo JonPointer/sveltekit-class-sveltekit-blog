f1b98ad30904a435cb74ad93ce0f640e7ba8ad91
Jon Pointer <jonpointer@msn.com>
2023-03-31 11:20:08 -0400

    After lesson 19

---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,25 @@
+<form method="POST">
+	<!-- POST means result is sent to +page.server.js file in same route directory  -->
+	<label>
+		Name: <input type="text" required name="name" id="name" />
+	</label>
+	<label>
+		Email: <input type="email" required name="email" id="email" />
+	</label>
+	<label>
+		Message: <textarea required name="message" id="message" />
+	</label>
+	<button type="submit">Send Email</button>
+</form>
+
+<style>
+	form {
+		padding: 20px;
+		display: flex;
+		gap: 20px;
+		flex-direction: column;
+	}
+	label {
+		display: block;
+	}
+</style>
