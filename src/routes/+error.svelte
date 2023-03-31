3b866a88d50b530db530d82c13dacf7712887526
Jon Pointer <jonpointer@msn.com>
2023-03-31 10:17:44 -0400

    After lesson 17

---
+++ src/routes/+error.svelte
@@ -0,0 +1,8 @@
+<script>
+	import { page } from '$app/stores';
+	$: console.log('Page: ', $page);
+</script>
+
+<h1>Oops!</h1>
+<p>{$page.status}</p>
+<p>{$page.error.message}</p>
--- src/routes/show/[num]/+page.server.js
+++ src/routes/show/[num]/+page.server.js
@@ -1,8 +1,15 @@
+import {error} from "@sveltejs/kit";
+
 export async function load ({fetch, params, setHeaders, locals}) {
     console.log('locals:', locals);
     
     const res=await fetch(`https://syntax.fm/api/shows/${params.num}`);
     const data=await res.json();
+    if (data.message) {
+        throw error (404, {
+            message: data.message
+        });
+    }
 
     setHeaders ({
         'Cache-Control': 'max-age=60'
