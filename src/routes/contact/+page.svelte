35b2c4543d9ad4f185e82735da636ff819bf75fa
Jon Pointer <jonpointer@msn.com>
2023-04-03 19:17:48 -0400

    After lesson 23

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,8 +1,25 @@
 <script>
-	import { enhance } from '$app/forms';
+	import { invalidateAll } from '$app/navigation'; // only needed for the crazy handleForm below when we took over control of everything. Normally don't do this.
+	import { applyAction, deserialize, enhance } from '$app/forms';
 
 	export let form;
 	$: console.log('form: ', form);
+
+	async function handleForm(event) {
+		// for the second form where we are taking over everything ourself.
+		// normally I wouldn't do any of this. This is all from lesson 23
+		// this = form element
+		const data = new FormData(this);
+		const res = await fetch(this.action, {
+			method: 'POST',
+			body: data
+		});
+		const result = deserialize(await res.text());
+		if (result.type === 'success') {
+			await invalidateAll();
+		}
+		applyAction(result);
+	}
 </script>
 
 <!-- Fail on role including admin -->
@@ -18,7 +35,7 @@
 		{form.message}
 	</p>
 {:else}
-	<form
+	<!-- <form
 		use:enhance={({ form, data, action, cancel }) => {
 			// form = form element
 			// data = FormData object
@@ -30,9 +47,10 @@
 				// update() = runs all of the normal default use:enhance functionality
 			};
 		}}
-		action="?/email"
+		action="/contact?/email" "route + ? + / + action_name" if in same folder, route not needed, just ?/action_name
 		method="POST"
-	>
+	> -->
+	<form on:submit|preventDefault={handleForm} action="/contact?/email">
 		<!-- POST means result is sent to +page.server.js file in same route directory  -->
 		<label>
 			Name: <input type="text" required name="name" id="name" />
